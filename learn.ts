b0bc7995bd19db0243a388915aefc46812fc6378
slimekepha <birirgudboy@gmail.com>
2026-08-14 01:24:00 +0300

    special types in ts

--- learn.ts
+++ learn.ts
@@ -9,3 +9,22 @@ let marks: number=6;
 let nm: string="john";
 let mrks: number=3.6;
 console.log(mrks);
+
+
+//using "any" in typecript
+
+let v:any=true;
+v="bacon";
+Math.round(v); // doesnt bring an error
+// to be avoided if possible
+
+//using uknown in type script
+
+function processvalue(value:unknown){
+    if(typeof value === 'string'){ // treats value as string
+        console.log(value.toUpperCase());
+    }else if(typeof value === "number"){ // treats value as number
+        console.log(Math.log(value));
+    }
+}
+// unknown is used whn dealing with data from external sources e,g APIs
