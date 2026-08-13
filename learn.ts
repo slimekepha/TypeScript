d5aab398c60c328651e877e1df3275242a1ef3a4
slimekepha <birirgudboy@gmail.com>
2026-08-14 00:58:16 +0300

    first commit

---
+++ learn.ts
@@ -0,0 +1,11 @@
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+}
+const message: string = greet("World");
+console.log(message);
+
+
+let marks: number=6;
+let nm: string="john";
+let mrks: number=3.6;
+console.log(mrks);
